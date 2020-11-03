270779ad615477e8cac86bde40597226a1081126
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2020-11-03 18:03:04 +0400

    add Edit

---
+++ EmployeeManagement/EmployeeManagement/ViewModels/EmployeeCreateViewModel.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class EmployeeCreateViewModel
+    {
+       
+        [Required]
+        [MaxLength(20, ErrorMessage = "Name cannot Exceed 20 characters")]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public Dept? Department { get; set; }
+        public IFormFile Photo { get; set; }
+    }
+}
