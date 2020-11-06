45d3fd0ddf2bc70cbf026af3a987e3d703203348
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2020-11-06 16:43:27 +0400

    Add Edit httppost

--- EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -19,6 +19,24 @@ namespace EmployeeManagement.Controllers
             this.hostingEnvironment = hostingEnvironment;
         }
 
+        private string ProcessUploadedFile(EmployeeCreateViewModel model)
+        {
+            string uniqueFileName = null;
+
+            if (model.Photo != null)
+            {
+                string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.Photo.CopyTo(fileStream);
+                }
+            }
+
+            return uniqueFileName;
+        }
+
         public ViewResult Index()
         {
             var model= _employeeRepository.GetAllEmployee();
@@ -95,6 +113,52 @@ namespace EmployeeManagement.Controllers
                 ExistingPhotoPath = employee.PhotoPath
             };
             return View(employeeEditViewModel);
+        }
+
+        // Through model binding, the action method parameter
+        // EmployeeEditViewModel receives the posted edit form data
+        [HttpPost]
+        public IActionResult Edit(EmployeeEditViewModel model)
+        {
+            // Check if the provided data is valid, if not rerender the edit view
+            // so the user can correct and resubmit the edit form
+            if (ModelState.IsValid)
+            {
+                // Retrieve the employee being edited from the database
+                Employee employee = _employeeRepository.GetEmployee(model.Id);
+                // Update the employee object with the data in the model object
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Department = model.Department;
+
+                // If the user wants to change the photo, a new photo will be
+                // uploaded and the Photo property on the model object receives
+                // the uploaded photo. If the Photo property is null, user did
+                // not upload a new photo and keeps his existing photo
+                if (model.Photo != null)
+                {
+                    // If a new photo is uploaded, the existing photo must be
+                    // deleted. So check if there is an existing photo and delete
+                    if (model.ExistingPhotoPath != null)
+                    {
+                        string filePath = Path.Combine(hostingEnvironment.WebRootPath,
+                            "images", model.ExistingPhotoPath);
+                        System.IO.File.Delete(filePath);
+                    }
+                    // Save the new photo in wwwroot/images folder and update
+                    // PhotoPath property of the employee object which will be
+                    // eventually saved in the database
+                    employee.PhotoPath = ProcessUploadedFile(model);
+                }
+
+                // Call update method on the repository service passing it the
+                // employee object to update the data in the database table
+                Employee updatedEmployee = _employeeRepository.Update(employee);
+
+                return RedirectToAction("index");
+            }
+
+            return View(model);
         }
     }
 }
